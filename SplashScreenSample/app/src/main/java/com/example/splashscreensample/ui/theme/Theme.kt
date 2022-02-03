8739aeaa08da6eb61f3105f157895b5f806f78cd
Ahmed Siddique <ahmed.siddique@citrusbits.com>
2022-02-03 17:48:58 +0500

    splash screen integrated. basic viewmodel

--- SplashScreenSample/app/src/main/java/com/example/splashscreensample/ui/theme/Theme.kt
+++ SplashScreenSample/app/src/main/java/com/example/splashscreensample/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun SplashScreenSampleTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun SplashScreenSampleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
